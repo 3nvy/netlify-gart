8df83cb51cb07bcf5c4fab2e52f454ff0e14ee2c
Renato Pestana <renatompestana@gmail.com>
2022-07-21 23:58:07 +0100

    Amend Apollo Client Uri

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
+    uri: "/graphql",
     cache: new InMemoryCache(),
 });
 
